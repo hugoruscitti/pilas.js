f2d00671e1de37645158a9cfa23e968f15e04419
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-10-05 16:59:41 -0300

    Arreglo menor.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
                 options: {
                     module: 'commonjs',
                     out: './docs',
-                    name: 'super-editor-reactivo',
+                    name: 'pilasengine.js',
                     target: 'es5'
                 },
                 src: ['./src/**/*']
